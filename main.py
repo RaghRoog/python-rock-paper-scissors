4c8f45eab0ebff886d28ccf7f4adea6b3e2ef35e
RaghRoog <szumon612@interia.pl>
2023-10-05 20:58:45 +0200

    deleted unnecessary new lines

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import random
 
-print("This is rock, paper scissors game.\n")
-print("Press 1 to see rules\n")
-print("Press 2 to play\n")
+print("This is rock, paper scissors game.")
+print("Press 1 to see rules")
+print("Press 2 to play")
 playerSelection = int(input())
 
 if playerSelection == 1:
-    print("Rules:\n")
-    print("Rock vs paper --> paper wins\n")
-    print("Rock vs scissors --> rock wins\n")
-    print("Paper vs scissors --> scissors wins\n")
-    print("To win you need to colect 3 points (win 3 rounds)\n")
+    print("Rules:")
+    print("Rock vs paper --> paper wins")
+    print("Rock vs scissors --> rock wins")
+    print("Paper vs scissors --> scissors wins")
+    print("To win you need to colect 3 points (win 3 rounds)")
 elif playerSelection == 2:
     possibleSelecetions = ["rock", "paper", "scissors"]
     cpuSelection = random.choice(possibleSelecetions)
     print("1. Rock\n2. Paper\n3. Scissors")
-    playerSelection = possibleSelecetions[int(input("Select move:\n"))-1]
+    playerSelection = possibleSelecetions[int(input("Select move:"))-1]
     if ((playerSelection == "rock" and cpuSelection == "scissors") or
         (playerSelection == "paper" and cpuSelection == "rock") or
         (playerSelection == "scissors" and cpuSelection == "paper")):
