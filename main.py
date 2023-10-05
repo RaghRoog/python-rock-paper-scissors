0f12ced01a74b2a8a387995ece7c42238bd54f41
RaghRoog <szumon612@interia.pl>
2023-10-05 21:19:16 +0200

    game is running in loop

--- main.py
+++ main.py
@@ -16,19 +16,28 @@ while(isLooped):
         print("Paper vs scissors --> scissors wins")
         print("To win you need to colect 3 points (win 3 rounds)")
     elif playerSelection == 2:
+        gameLoop = True
+        cpuPoints = 0
+        playerPoints = 0
         possibleSelecetions = ["rock", "paper", "scissors"]
-        cpuSelection = random.choice(possibleSelecetions)
-        print("1. Rock\n2. Paper\n3. Scissors")
-        playerSelection = possibleSelecetions[int(input("Select move:"))-1]
-        if ((playerSelection == "rock" and cpuSelection == "scissors") or
-            (playerSelection == "paper" and cpuSelection == "rock") or
-            (playerSelection == "scissors" and cpuSelection == "paper")):
-                print("You: " + playerSelection + " vs CPU: " + cpuSelection)
-                print("You won")
-        elif ((playerSelection == "rock" and cpuSelection == "paper") or
-              (playerSelection == "paper" and cpuSelection == "scissors") or
-              (playerSelection == "scissors" and cpuSelection == "rock")):
-                print("You: " + playerSelection + " vs CPU: " + cpuSelection)
-                print("You lost")
+        while(gameLoop):
+            cpuSelection = random.choice(possibleSelecetions)
+            print("1. Rock\n2. Paper\n3. Scissors")
+            playerSelection = possibleSelecetions[int(input("Select move: "))-1]
+            if ((playerSelection == "rock" and cpuSelection == "scissors") or
+                (playerSelection == "paper" and cpuSelection == "rock") or
+                (playerSelection == "scissors" and cpuSelection == "paper")):
+                    print("You: " + playerSelection + " vs CPU: " + cpuSelection)
+                    print("You won this round")
+                    playerPoints += 1
+            elif ((playerSelection == "rock" and cpuSelection == "paper") or
+                  (playerSelection == "paper" and cpuSelection == "scissors") or
+                  (playerSelection == "scissors" and cpuSelection == "rock")):
+                    print("You: " + playerSelection + " vs CPU: " + cpuSelection)
+                    print("You lost")
+                    cpuPoints += 1
+            print("Your points: " + str(playerPoints) + " CPU points: " + str(cpuPoints))
+            if playerPoints == 3 or cpuPoints == 3:
+                gameLoop = False
     elif playerSelection == 3:
         isLooped = False
