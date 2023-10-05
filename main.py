1b9c289cec7b80c187b247f18347def29079cf2b
RaghRoog <szumon612@interia.pl>
2023-10-05 20:24:44 +0200

    rules screen

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 print("This is rock, paper scissors game.\n")
 print("Press 1 to see rules\n")
 print("Press 2 to play\n")
-playerSelection = input()
+playerSelection = int(input())
+
+if playerSelection == 1:
+    print("Rules:\n")
+    print("Rock vs paper --> paper wins\n")
+    print("Rock vs scissors --> rock wins\n")
+    print("Paper vs scissors --> scissors wins\n")
+    print("To win you need to colect 3 points (win 3 rounds)\n")
